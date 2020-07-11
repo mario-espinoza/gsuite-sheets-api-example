f3da3561f7826a81f8a686f73e2c3d5536589159
mespinozas <mario@polinom.io>
2020-07-11 12:13:33 -0400

    updated example

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # G Suite Sheets API example
 
+Original Colelab [here](https://codelabs.developers.google.com/codelabs/sheets-api)
+
+Updated example to:
+
+- GoogleApis 52.x.x
+- Google Auth library 6.x.x
+
 OAUTH script consent to internal to use the app inside your company
+
+## Possible improvements
+
+Here are some additional ideas for making an even more compelling integration:
+
+### Automatic sync
+
+Rather than have the user click a button to sync, update the spreadsheets automatically whenever orders are changed. You'd need to request [offline access](https://developers.google.com/identity/protocols/OAuth2WebServer#offline) from your users, and keep track of which user owns a given spreadsheet.
+
+### Two-way sync
+
+Allow users to modify order details in the spreadsheet and push those changes back into the application. You could use [Google Drive push notifications](https://developers.google.com/drive/v3/web/push) to detect when the spreadsheet has been updated and the Sheets API's [spreadsheets.values.get](https://developers.google.com/sheets/reference/rest/v4/spreadsheets.values/get) to pull in the updated data.
+
+## Learn More
+
+Read the Google Sheets API developer documentation.
+Post questions and find answers on Stackoverflow under the google-sheets-api tag.
--- routes.js
+++ routes.js
@@ -25,27 +25,28 @@ var models = require('./models');
 var Sequelize = require('sequelize');
 
 // TODO: Show spreadsheets on the main page.
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   var options = {
     order: [['createdAt', 'DESC']],
     raw: true
   };
-  models.Order.findAll(options)
-  .then(function(orders) {
+  Sequelize.Promise.all([
+    models.Order.findAll(options),
+    models.Spreadsheet.findAll(options)
+  ]).then(function (results) {
     res.render('index', {
-      orders: orders
+      orders: results[0],
+      spreadsheets: results[1]
     });
-  }, function(err) {
-    next(err);
   });
 });
 
-router.get('/create', function(req, res, next) {
+router.get('/create', function (req, res, next) {
   res.render('upsert');
 });
 
-router.get('/edit/:id', function(req, res, next) {
-  models.Order.findByPk(req.params.id).then(function(order) {
+router.get('/edit/:id', function (req, res, next) {
+  models.Order.findByPk(req.params.id).then(function (order) {
     if (order) {
       res.render('upsert', {
         order: order.toJSON()
@@ -56,35 +57,78 @@ router.get('/edit/:id', function(req, res, next) {
   });
 });
 
-router.get('/delete/:id', function(req, res, next) {
+router.get('/delete/:id', function (req, res, next) {
   models.Order.findByPk(req.params.id)
-    .then(function(order) {
+    .then(function (order) {
       if (!order) {
         throw new Error('Order not found: ' + req.params.id);
       }
       return order.destroy();
     })
-    .then(function() {
+    .then(function () {
       res.redirect('/');
-    }, function(err) {
+    }, function (err) {
       next(err);
     });
 });
 
-router.post('/upsert', function(req, res, next) {
-  models.Order.upsert(req.body).then(function() {
+router.post('/upsert', function (req, res, next) {
+  models.Order.upsert(req.body).then(function () {
     res.redirect('/');
-  }, function(err) {
+  }, function (err) {
     next(err);
   });
 });
 
 // TODO: Add route for creating spreadsheet.
+var SheetsHelper = require('./sheets');
 
+router.post('/spreadsheets', function (req, res, next) {
+  var auth = req.get('Authorization');
+  if (!auth) {
+    return next(Error('Authorization required.'));
+  }
+  var accessToken = auth.split(' ')[1];
+  var helper = new SheetsHelper(accessToken);
+  var title = 'Orders (' + new Date().toLocaleTimeString() + ')';
+  helper.createSpreadsheet(title, function (err, spreadsheet) {
+    if (err) {
+      return next(err);
+    }
+    var model = {
+      id: spreadsheet.spreadsheetId,
+      sheetId: spreadsheet.sheets[0].properties.sheetId,
+      name: spreadsheet.properties.title
+    };
+    models.Spreadsheet.create(model).then(function () {
+      return res.json(model);
+    });
+  });
+});
 
 
 // TODO: Add route for syncing spreadsheet.
 
-
+router.post('/spreadsheets/:id/sync', function (req, res, next) {
+  var auth = req.get('Authorization');
+  if (!auth) {
+    return next(Error('Authorization required.'));
+  }
+  var accessToken = auth.split(' ')[1];
+  var helper = new SheetsHelper(accessToken);
+  Sequelize.Promise.all([
+    models.Spreadsheet.findByPk(req.params.id),
+    models.Order.findAll()
+  ]).then(function (results) {
+    var spreadsheet = results[0];
+    var orders = results[1];
+    helper.sync(spreadsheet.id, spreadsheet.sheetId, orders, function (err) {
+      if (err) {
+        return next(err);
+      }
+      return res.json(orders.length);
+    });
+  });
+});
 
 module.exports = router;
